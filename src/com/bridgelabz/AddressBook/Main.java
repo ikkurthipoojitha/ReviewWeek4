d5e05464e0d1806c70b46216c6d073329a5b87ac
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-06-29 09:18:01 +0530

    added

--- src/com/bridgelabz/AddressBook/Main.java
+++ src/com/bridgelabz/AddressBook/Main.java
@@ -1,11 +1,23 @@
 package com.bridgelabz.AddressBook;
 
+import javax.naming.CompositeName;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
 
-        AddressBook addressBook1 = new AddressBook();
-        AddressBook addressBook2 = new AddressBook();
-        AddressBook addressBook3 = new AddressBook();
+        List<ContactList> listContact = new ArrayList<ContactList>();
+
+        ContactList contactList1 = new ContactList("a","b","guntur");
+        ContactList contactList2 = new ContactList("g","j","Hyderabad");
+        ContactList contactList3 = new ContactList("LA","hj","kolkaata");
+
+
+        listContact.add(contactList1);
+        listContact.add(contactList2);
+        listContact.add(contactList3);
 
 
     }
